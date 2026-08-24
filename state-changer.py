6184c42b1d82c871ed80329b4814aadba31ce453
poury <poury.2006.1385@gmail.com>
2026-08-24 13:03:14 +0430

    tried to make state-changer neater

--- state-changer.py
+++ state-changer.py
@@ -4,17 +4,21 @@ def Doner(id):
     with open("s.json" , "r") as f:
         try:
             data = json.load(f)
+
         except (json.JSONDecodeError):
             return False
+
     try:
         (data[id]["Done"]) = True
+
     except(IndexError):
         return False
+
     else:
         with open("s.json" , 'w') as ff:
             json.dump(data , ff)
         return True
 
 
-if(Doner(1)):         #for test
-    print("ok")
+# if(Doner(1)):         #for test
+#     print("ok")
