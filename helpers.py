bfada224f1c27dff4e0e10c3128e643335df81da
poury <poury.2006.1385@gmail.com>
2026-09-15 15:14:13 +0430

    added helpers

---
+++ helpers.py
@@ -0,0 +1,11 @@
+def GoToJson(data):
+    with open("s.json" , "w") as ff:
+        json.dump(data , ff , indent = 4)
+
+def getdata():
+    with open("s.json" , "r") as ff:
+        try:
+            data = json.load(ff)
+        except (json.JSONDecodeError):
+            return False
+    return data
